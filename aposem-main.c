80dd91d5ef9a812f17137bb7e8f8d99728532d89
Petr Lendvorsky <lendvpet@fel.cvut.cz>
2021-05-14 14:34:57 +0200

    Added player and ball structures

--- aposem-main.c
+++ aposem-main.c
@@ -20,6 +20,14 @@ int main(int argc, char *argv[]){
   if(display.lcd_membase==NULL || led_membase==NULL)
     exit(EXIT_FAILURE);
 
+  display.player1.nick = (argc>1)?argv[1]:"Player_1";
+  display.player2.nick = (argc>2)?argv[1]:"Player_2";
+
+  bool game = true;
+  while(game){
+    
+  }
+
   parlcd_hx8357_init(display.lcd_membase);
 
   draw_display(&display);
--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
@@ -15,16 +16,24 @@
 
 //#define _POSIX_C_SOURCE 200112L
 
+typedef struct ball{
+  int posx, posy;
+  char color;
+} ball;
+
 typedef struct player {
-  char nick[10];
+  char* nick;
   bool winner;
   char color;
   int score;
+  int position;   //player position 0-(sizey-size)
+  int size;       //player size 
 } player;
 
 typedef struct display_data {
   unsigned char* lcd_membase;
   player player1, player2;
+  int sizex, sizey;
 } display_data;
 
 #endif
